6e17db3a182cf1e1154482dd75ebaf672a2cf50a
Vova <vovalak@mail.ru>
2020-04-12 22:19:41 +0300

    add firebase

--- src/main.js
+++ src/main.js
@@ -8,14 +8,37 @@ import dateFilter from '@/filters/date.filter'
 import messagePlugin from '@/utils/message.plugin'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const configDB = {
+  apiKey: "AIzaSyDO9z9J9aZ7uXqCQzOfyBvDAd_tqISxCm4",
+  authDomain: "vue-crm-training.firebaseapp.com",
+  databaseURL: "https://vue-crm-training.firebaseio.com",
+  projectId: "vue-crm-training",
+  storageBucket: "vue-crm-training.appspot.com",
+  messagingSenderId: "152266731608",
+  appId: "1:152266731608:web:f103eed95fffaa77451a2d",
+  measurementId: "G-MR49MHNRPB"
+}
+
+firebase.initializeApp(configDB)
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,7 +7,7 @@
           id="email"
           type="text"
           v-model.trim="email"
-          :class="{invalid: ($v.email.$dirty && !$v.email.required) ||  ($v.email.$dirty && !$v.email.email)}" />
+          :class="{invalid: ($v.email.$dirty && !$v.email.required) ||  ($v.email.$dirty && !$v.email.email)}"/>
         <label for="email">Email</label>
         <small
           class="helper-text invalid"
@@ -32,7 +32,8 @@
         <small
           class="helper-text invalid"
           v-else-if="$v.password.$dirty && !$v.password.minLength"
-        >Пароль должен быть минимум {{$v.password.$params.minLength.min}} символов. Сейчас он {{password.length}}</small>
+        >Пароль должен быть минимум {{$v.password.$params.minLength.min}} символов. Сейчас он
+          {{password.length}}</small>
       </div>
       <div class="input-field">
         <input
@@ -47,11 +48,11 @@
           v-if="$v.name.$dirty && !$v.name.required"
         >
           Введите ваше имя
-		</small>
+        </small>
       </div>
       <p>
         <label>
-          <input type="checkbox" v-model="agree" />
+          <input type="checkbox" v-model="agree"/>
           <span>С правилами согласен</span>
         </label>
       </p>
@@ -59,8 +60,8 @@
     <div class="card-action">
       <div>
         <button
-            class="btn waves-effect waves-light auth-submit"
-            type="submit"
+          class="btn waves-effect waves-light auth-submit"
+          type="submit"
         >
           Зарегистрироваться
           <i class="material-icons right">send</i>
@@ -76,37 +77,37 @@
 </template>
 
 <script>
-import {email, required, minLength} from 'vuelidate/lib/validators'
+  import {email, required, minLength} from 'vuelidate/lib/validators'
 
-export default {
-  name: 'register',
-  data: () => ({
-    email: '',
-	password: '',
-	name: '',
-	agree: false
-  }),
-  validations: {
-    email: {email, required},
-	password: {required, minLength: minLength(6)},
-	name: {required},
-	agree: {checked: v => v},
-  },
-  methods: {
-    submitHandler() {
-      if(this.$v.$invalid) {
-        this.$v.$touch()
-        return
-	  }
+  export default {
+    name: 'register',
+    data: () => ({
+      email: '',
+      password: '',
+      name: '',
+      agree: false
+    }),
+    validations: {
+      email: {email, required},
+      password: {required, minLength: minLength(6)},
+      name: {required},
+      agree: {checked: v => v},
+    },
+    methods: {
+      submitHandler() {
+        if (this.$v.$invalid) {
+          this.$v.$touch()
+          return
+        }
 
-      const formData = {
-        email: this.email,
-        password: this.password,
-        name: this.name
-	  }
+        const formData = {
+          email: this.email,
+          password: this.password,
+          name: this.name
+        }
 
-      this.$router.push('/');
+        this.$router.push('/');
+      }
     }
   }
-}
 </script>
