1d01ca0b6ff834117ad86efb0d40913b4e88a31f
Vova <vovalak@mail.ru>
2020-05-04 23:17:28 +0300

    expense schedule

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading" />
@@ -34,12 +34,61 @@
 <script>
 import HistoryTable from '@/components/HistoryTable'
 import paginationMixin from '@/mixins/pagination.mixin'
+import { Pie } from 'vue-chartjs'
 
 export default {
   name: 'history',
-  mixins: [paginationMixin],
+  mixins: [paginationMixin, Pie],
   components: {
     HistoryTable
+  },
+  methods: {
+    setup(categories) {
+      this.setupPagination(
+        this.records.map(record => {
+          return {
+            ...record,
+            categoryName: categories.find(
+              category => category.id === record.categoryId
+            ).title,
+            typeClass: record.type === 'income' ? 'green' : 'red',
+            typeText: record.type === 'income' ? 'Доход' : 'Расход'
+          }
+        })
+      )
+
+      this.renderChart({
+        labels: categories.map(category => category.title),
+        datasets: [{
+            label: 'Расходы по категориям',
+            data: categories.map(category => {
+              return this.records.reduce((total, record) => {
+                if (record.categoryId === category.id && record.type === 'outcome') {
+                  total += +record.amount
+                }
+                return total
+              }, 0)
+            }),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+      })
+    }
   },
   data: () => ({
     loading: true,
@@ -49,16 +98,9 @@ export default {
     this.records = await this.$store.dispatch('fetchRecords')
     const categories = await this.$store.dispatch('fetchCategories')
 
-    this.setupPagination(this.records.map(record => {
-      return {
-        ...record,
-        categoryName: categories.find(category => category.id === record.categoryId).title,
-        typeClass: record.type === 'income' ? 'green' : 'red',
-        typeText: record.type === 'income' ? 'Доход' : 'Расход',
-      }
-    }))
+    this.setup(categories)
 
     this.loading = false
-  },
+  }
 }
 </script>
